328d475f48a2ea25c2c5e263088075efdd0ea49c
hexaflix <107338765+hexaflix@users.noreply.github.com>
2022-06-13 16:59:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -754,7 +754,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('♻️ᴀʙᴏᴜᴛ♻️', callback_data='about')
             ],[
             InlineKeyboardButton('ɴᴇᴡ ʀᴇʟᴇᴀꜱᴇᴅ ᴍᴏᴠɪᴇꜱ', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
-          
+        ]]  
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
